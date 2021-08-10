4e5afbab50d71c41580c9816b72ef9e863e8534c
Talha Yousuf <talhayousuf.work@gmail.com>
2021-08-10 22:39:52 +0500

    ...

--- src/components/FeaturedProjects/FeaturedProjects.js
+++ src/components/FeaturedProjects/FeaturedProjects.js
@@ -5,7 +5,6 @@ import { Section } from "components/Common/SectionStyles";
 const FeaturedProjects = () => (
   <div>
     <Section fullHeight nopadding>
-      <div style={{ height: "7rem" }} />
       Feature Project
     </Section>
   </div>
