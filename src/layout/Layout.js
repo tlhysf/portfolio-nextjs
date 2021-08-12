f636f230dead1046cbe25358889238c747d1b331
Talha Yousuf <talhayousuf.work@gmail.com>
2021-08-13 02:57:46 +0500

    close sidebar on hash change

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbar/Navbar";
@@ -9,10 +9,28 @@ import { Container } from "layout/LayoutStyles";
 
 export const Layout = ({ children, scroll }) => {
   const [openSidebar, setOpenSidebar] = useState(false);
+  const [hash, setHash] = useState(null);
 
   const toggleSidebar = () => {
     setOpenSidebar(!openSidebar);
   };
+
+  const handleHashChange = (hash) => {
+    const onHashChange = () => {
+      setOpenSidebar(false);
+      return hash;
+    };
+
+    setHash((state) => (hash === state ? state : onHashChange()));
+  };
+
+  useEffect(() => {
+    window.addEventListener(
+      "onhashchange",
+      handleHashChange(window.location.hash),
+      true
+    );
+  });
 
   return (
     <Container>
