dec4f96bd424e1caeefe36b0aa8c9774bb604775
tlhysf <talhayousuf.work@gmail.com>
2023-04-09 23:30:23 +0500

    skills updated

--- src/data/about.js
+++ src/data/about.js
@@ -4,34 +4,15 @@ export const about = [
   "I am interested in working as either a Full-stack or Front-end Developer, preferably in React and Node but I am also open to exploring other technologies.",
 ];
 
-export const skills = [
-  {
-    title: "Languages",
-    list: "JavaScript(ES6), HTML + CSS, Python, C++",
-  },
-  {
-    title: "Libraries & Frameworks",
-    list: "ReactJS, NodeJS, Redux, NextJS, ExpressJS, Styled Components, MaterialUI",
-  },
-  {
-    title: "Tools & Platforms",
-    list: "Git, Netlify, Heroku, Firebase, MongoDB",
-  },
-  {
-    title: "Design",
-    list: "Figma, Photoshop",
-  },
-];
-
 export const skillsAlt = [
-  "JavaScript (ES6)",
+  "JavaScript",
   "HTML, CSS",
   "React, Redux, Next",
-  "Styled Components, MaterialUI, Bootstrap",
+  "TailwindCss, Styled Components, MaterialUI",
   "Node, Express",
-  "MongoDB, MySQL",
-  "Netlify, Heroku, Firebase",
-  "Git, Trello, Redmine",
-  "Figma, Photoshop",
-  "Python, C++",
+  "MongoDB, Firebase",
+  "Jenkins, Docker",
+  "Bash",
+  "Python",
+  "Figma",
 ];
