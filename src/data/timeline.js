6aa476914a3f616a0f5974be4869dbb7dfeee8f2
tlhysf <talhayousuf.work@gmail.com>
2023-04-09 23:49:25 +0500

    timeline updated

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,18 +1,39 @@
 export const timeline = [
   {
-    period: "April 2021 - Present",
+    period: "Jan 2023 - Present",
+    title: "Senior Software Engineer",
+    place: "Neoteric Softwares (Pvt) Ltd.",
+    description:
+      "A subsidiary of Sedenius Engineering GmbH, focused on web development.",
+    bullets: [
+      "Worked on a peer-to-peer NFT marketplace web app from concept to completion. My contribution was in developing the, 1) front-end for the customer facing app which allows users to mint, purchase and sell NFTs over multiple blockchains, and 2) a CMS app for the admin. The tech stack consisted of react, redux, and tailwindCSS.",
+      "Performed planning and estimation for multiple projects.",
+      "Planned and carried out refactoring tasks for performance improvements.",
+      "Built scalable, high-volume, high-availability systems, with a focus on delivering customer value.",
+      "Performed peer reviews and coached junior developers.",
+      "Worked on containerization of multiple applications.",
+      "Created and maintained CI/CD pipelines.",
+    ],
+    link: "https://sedenius.com/",
+  },
+  {
+    period: "April 2021 - Jan 2023",
     title: "MERN Stack Developer",
     place: "Sedenius Technologies (Pvt) Ltd.",
     description:
       "A subsidiary of Sedenius Engineering GmbH, focused on software development for automotive industry.",
     bullets: [
-      "Developed an asset management and rental web portal using React, Redux, and, LeafletJS.",
-      "Closely worked with product manager and design team to bring the application from concept to code.",
+      "Developed an asset management and rental web portal using React and Redux.",
+      "Developed a coding assessment platform using react and ϐirebase.",
+      "Worked on a smart home devices app (IoT) built in React Native (iOS, Android). I worked on implementing some new features, resolving bugs, and code-refactoring.",
+      "Closely worked with the product manager and design team to bring the applications from concept to code.",
       "Performed manual testing in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness.",
-      "Set up and managed CI/CD pipeline with Jenkins.",
-      "Implemented new features, resolved bugs, improved performance through code-refactoring for a customer-facing react native app.",
+      "Designed & developed new, as well as maintained legacy backend services and REST APIs written in Javascript (NodeJS).",
+      "Developed internal tools and automation scripts in Bash & Python.",
+      "Responsible for creating and maintaining CI/CD pipelines for projects. This involved setting up Jenkins, writing bash scripts for automated testing, server deployment, and managing the server environment.",
+      "Worked on making applications EU GDPR regulations compliant.",
     ],
-    link: "https://pk.sedenius.com/",
+    link: "https://sedenius.com/",
   },
   {
     period: "July 2019 - April 2021",
@@ -24,25 +45,25 @@ export const timeline = [
       "Developed and shipped highly customized and fully-responsive enterprise IoT Web portals built in MERN Stack.",
       "Interfaced with clients on a weekly basis for requirement gathering.",
       "Worked closely with designers for translating business requirements into technical features and designing the UI/UX.",
-      "Worked with a team of developers to design the backend architecture.",
       "Translated UI wireframes to frontend code.",
-      "Worked on code-refactoring, bug-fixing and developing new features for existing applications (mainly in JavaScript).",
-      "Learned and implemented best development practices to write clean, re-useable, scalable, and efficient code.",
-      "Developed and maintained RESTFul web services.",
+      "Designed & developed new, as well as maintained legacy backend services and REST APIs written in Javascript (NodeJS).",
+      "Developed internal tools and automation scripts in Bash & Python.",
+      "Build data pipelines in python and sqlite to clean and organize IoT data from different sources and generate visualizations.",
+      "Worked on embedded hardware and software for IoT sensor nodes from PCB design to fabrication to real-world deployment and testing.",
     ],
     link: "https://www.carepvtltd.com/",
   },
-  {
-    period: "April 2018 - Sep 2019",
-    title: "Embedded Systems Developer",
-    place: "Genes and Machines",
-    description:
-      "A startup focused on developing an interdisciplinary knowledge community in Pakistan for Life Sciences.",
-    bullets: [
-      "Developed embedded hardware and software for point-of-care blood testing devices.",
-    ],
-    // link: "https://www.facebook.com/GenesMachines.io",
-  },
+  // {
+  //   period: "April 2018 - Sep 2019",
+  //   title: "Embedded Systems Developer",
+  //   place: "Genes and Machines",
+  //   description:
+  //     "A startup focused on developing an interdisciplinary knowledge community in Pakistan for Life Sciences.",
+  //   bullets: [
+  //     "Developed embedded hardware and software for point-of-care blood testing devices.",
+  //   ],
+  //   // link: "https://www.facebook.com/GenesMachines.io",
+  // },
   {
     period: "Feb 2018 - Sep 2019",
     title: "Co-founder/Embedded Systems Developer",
@@ -60,5 +81,7 @@ export const timeline = [
     title: "Bachelors of Electrical Engineering",
     place: "NUST",
     link: "https://nust.edu.pk/",
+    description:
+      "Relevant classes: Data Structures & Algorithms, Object Oriented Programming, Embedded System Design",
   },
 ];
