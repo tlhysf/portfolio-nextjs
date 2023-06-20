892b99b18f259b518a9988a569f4d749e4ad6cff
tlhysf <talhayousuf.work@gmail.com>
2023-06-21 03:36:44 +0500

    firebase config moved to env

--- src/data/images.js
+++ src/data/images.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 
-const config = {
-  apiKey: "AIzaSyD19JTvn5HSoZYjBCqAVqqmusB01PtCWCE",
-  authDomain: "talha-yousuf.firebaseapp.com",
-  databaseURL: "https://talha-yousuf-default-rtdb.firebaseio.com",
-  projectId: "talha-yousuf",
-  storageBucket: "talha-yousuf.appspot.com",
-  messagingSenderId: "188227464815",
-  appId: "1:188227464815:web:7b20f33e83ab01630fa79a",
-  measurementId: "G-EPXP2LZJZH",
-};
+const config = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 const app = initializeApp(config);
 const storage = getStorage(app);
